1a38ae68b24027c83edb1e63fb781a62b2f37c87
nachewigkeit <1501793890@qq.com>
2021-11-01 22:24:54 +0800

    delete

--- DuplicateCheck/config.py
+++
@@ -1,4 +0,0 @@
-databasePath = r"data/database"
-oldTextPath = r"E:\AI\dataset\NLP\government\train"
-newTextPath = r"E:\AI\dataset\NLP\government\test\1.txt"
-datasetPath = r"E:\AI\dataset\NLP\lcqmc"
